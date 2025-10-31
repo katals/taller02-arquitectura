eaee140f5df9540fdba59530277b71e23d6f90e7
katals <juanca20042004@hotmail.com>
2025-10-30 23:52:10 -0500

    cambios

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # Para correr localmente: python wsgi.py
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
